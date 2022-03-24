0a3d5183e473145f89e4e9fed11ee597666831ee
koltrastH <elina.hulbert@hyperisland.se>
2022-03-24 15:38:40 +0100

    delete from cart

--- frontend/src/components/Drawer.js
+++ frontend/src/components/Drawer.js
@@ -1,46 +1,81 @@
-function Drawer({onCloseCart, items = []}){
-  console.log(items, " props in drawer")
+function Drawer({ onCloseCart, onRemove, items = [] }) {
+  console.log(items, " props in drawer");
   // const data = items.sneakers;
-    return(
+  return (
     <div className="overlay">
-    <div className="drawer">
-      <h2 className="d-flex justify-between mb-30">Cart <img className="removeBtn cu-p" src="/img/plusBold.svg" alt="Remove" onClick={onCloseCart}/></h2>
+      <div className="drawer">
+        <h2 className="d-flex justify-between mb-30">
+          Cart{" "}
+          <img
+            className="removeBtn cu-p"
+            src="/img/plusBold.svg"
+            alt="Remove"
+            onClick={onCloseCart}
+          />
+        </h2>
 
-      <div className="items">
-        {items.map((obj) => 
-        (<div className="cartItem d-flex align-center mb-20">
-          
-          <div className="cartItemImg" style={{backgroundImage: `url(${obj.imageUrl})`}}>
-          </div>
-          
-          <div className="mr-20 flex">
-            <p className="mb-5">{obj.title}</p>
-            <b>{obj.price} :-</b>
-          </div>
+        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+          <img
+            className="mb-20"
+            width="120px"
+            height="120px"
+            src="/img/empty-cart.jpg"
+            alt="empty box/cart"
+          />
+          <h2>Your cart is empty</h2>
+          <p className="opacity=6">
+            Add at least one pair of sneakers to be able to make an order
+          </p>
+          <button className="greenButton">
+            <img src="/img/arrow_back.svg" alt="arrow-back" />
+            Go back
+          </button>
+        </div>
 
-          <img className="removeBtn" src="/img/plusBold.svg" alt="Remove" />
-        </div>))}
+        <div className="items">
+          {items.map((obj) => (
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                className="cartItemImg"
+                style={{ backgroundImage: `url(${obj.imageUrl})` }}
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price} :-</b>
+              </div>
+
+              <img
+                onClick={() => onRemove(obj._id)}
+                className="removeBtn"
+                src="/img/plusBold.svg"
+                alt="Remove"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Total: </span>
+              <div></div>
+              <b>3200 kr</b>
+            </li>
+
+            <li>
+              <span>Tax 5%: </span>
+              <div></div>
+              <b>160 kr</b>
+            </li>
+          </ul>
+          <button className="greenButton">
+            Make your order <img src="/img/arrow.svg" alt="arrow" />
+          </button>
+        </div>
       </div>
-
-      <div className="cartTotalBlock">
-        <ul>
-          <li>
-            <span>Total: </span>
-            <div></div>
-            <b>3200 kr</b>
-          </li>
-
-          <li>
-            <span>Tax 5%: </span>
-            <div></div>
-            <b>160 kr</b>
-          </li>
-        </ul>
-        <button className="greenButton">Make your order <img src="/img/arrow.svg" alt="arrow" /></button>
-      </div>
-    
     </div>
-  </div>)
+  );
 }
 
 export default Drawer;
--- frontend/src/components/Render.js
+++ frontend/src/components/Render.js
@@ -1,54 +1,92 @@
-import { useEffect, useState } from "react";
 import React from "react";
 import axios from "axios";
 import Card from "./Card/index.js";
 import Header from "./Header.js";
 import Drawer from "./Drawer.js";
 
-const Render = (props) =>{
-const data = props.sneakers;
-const [cartOpened, setCartOpened] = React.useState(false);
-const [cartItems, setCartItems] = React.useState([]);
-const [searchValue, setSearchValue] = React.useState("");
+const Render = (props) => {
+  const data = props.sneakers;
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
 
-const onAddToCart = (obj) => {
-  setCartItems(prev => [...prev, obj])
-  axios.post("http://localhost:8080/cart", obj);
-};
+  const onAddToCart = (obj) => {
+    axios.post("http://localhost:8080/cart", obj);
+    setCartItems((prev) => [...prev, obj]);
+  };
 
-const onChangeSearchInput = (event) => {
-  setSearchValue(event.target.value);
-}
-console.log(cartItems, " cartItems")
-return (
-  <div className="wrapper clear">
-    {cartOpened && <Drawer key={cartItems._id} items={cartItems} onCloseCart={() => setCartOpened(false)}/>}
-    <Header onClickCart={() => setCartOpened(true)} />
+  React.useEffect(() => {
+    axios.get("http://localhost:8080/cart").then((res) => {
+      setCartItems(res.data);
+    });
+  }, []);
 
-    <div className="content p-40">
-      <div className="d-flex align-center justify-between mb-40">
-        <h1>{searchValue ? `Search by input: "${searchValue}"` : "All Sneakers"}</h1>
-        <div className="search-block d-flex align-center">
-          <img width={16} height={16} src="/img/search.svg" alt="search" />
-          {searchValue && <img className="clear cu-p" src="/img/plusBold.svg" alt="Clear" onClick={() => setSearchValue("")}/>}
-          <input onChange={onChangeSearchInput} value={searchValue} placeholder="Search..." type="text" />
+  const onRemoveItem = (_id) => {
+    axios.delete(`http://localhost:8080/cart/${_id}`);
+    setCartItems((prev) => prev.filter((item) => item._id !== _id));
+  };
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  };
+  console.log(cartItems, " cartItems");
+  return (
+    <div className="wrapper clear">
+      {cartOpened && (
+        <Drawer
+          key={cartItems._id}
+          items={cartItems}
+          onCloseCart={() => setCartOpened(false)}
+          onRemove={onRemoveItem}
+        />
+      )}
+      <Header onClickCart={() => setCartOpened(true)} />
+
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>
+            {searchValue ? `Search by input: "${searchValue}"` : "All Sneakers"}
+          </h1>
+          <div className="search-block d-flex align-center">
+            <img width={16} height={16} src="/img/search.svg" alt="search" />
+            {searchValue && (
+              <img
+                className="clear cu-p"
+                src="/img/plusBold.svg"
+                alt="Clear"
+                onClick={() => setSearchValue("")}
+              />
+            )}
+
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder="Search..."
+              type="text"
+            />
+          </div>
+        </div>
+
+        <div className="d-flex flex-wrap">
+          {data
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((data) => (
+              <Card
+                key={data._id}
+                title={data.name}
+                price={data.price}
+                imageUrl={data.imageUrl}
+                _id={data._id}
+                onFavClick={() => console.log("Added to fav")}
+                onAddClick={(obj) => onAddToCart(obj)}
+                // onAddClick={(obj) => console.log(obj, " obj")}
+              />
+            ))}
         </div>
       </div>
-
-      <div className="d-flex flex-wrap "> 
-        {
-          data.filter(item=> item.name.toLowerCase().includes(searchValue.toLowerCase())).map((data) => (
-          <Card 
-            key = {data._id}
-            title={data.name} 
-            price={data.price} 
-            imageUrl={data.imageUrl} 
-            onFavClick={()=>console.log("Added to fav")}
-            onAddClick={(obj) => onAddToCart(obj)}/>))
-        }
-      </div> 
     </div>
-  </div>
-);
-}
+  );
+};
 export default Render;
